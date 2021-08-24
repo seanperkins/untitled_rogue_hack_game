d9bc7c43d6a66005b6a7df01beb68a2212915cec
Sean Perkins <sean@mobility-labs.com>
2021-08-24 13:47:59 -0400

    More or less the end of the roguelike tutorial 2020

--- __main__.py
+++ __main__.py
@@ -1,48 +1,65 @@
+#!/usr/bin/env python3
+import traceback
+
 import tcod
 
-from lib.actions import EscapeAction, MovementAction
-from lib.input_handlers import EventHandler
+import game.color as color
+import game.exceptions as exceptions
+import game.setup_game as setup_game
+import game.input_handlers as input_handlers
+
+
+def save_game(handler: input_handlers.BaseEventHandler, filename: str) -> None:
+    """If the current event handler has an active Engine then save it."""
+    if isinstance(handler, input_handlers.EventHandler):
+        handler.engine.save_as(filename)
+        print("Game saved.")
+
 
 def main() -> None:
-    screen_width = 80
-    screen_height = 50
-
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    screen_width = 100
+    screen_height = 60
 
     tileset = tcod.tileset.load_tilesheet(
-        "./assets/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+        "data/dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
-    event_handler = EventHandler()
+    handler: input_handlers.BaseEventHandler = setup_game.MainMenu()
 
     with tcod.context.new_terminal(
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Untitled Hacking Game",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
-        while True:
-            root_console.print(x=player_x, y=player_y, string="@")
-            
-            context.present(root_console)
+        try:
+            while True:
+                root_console.clear()
+                handler.on_render(console=root_console)
+                context.present(root_console)
 
-            root_console.clear()
+                try:
+                    for event in tcod.event.wait():
+                        context.convert_event(event)
+                        handler = handler.handle_events(event)
+                except Exception:  # Handle exceptions in game.
+                    traceback.print_exc()  # Print error to stderr.
+                    # Then print the error to the message log.
+                    if isinstance(handler, input_handlers.EventHandler):
+                        handler.engine.message_log.add_message(
+                            traceback.format_exc(), color.error
+                        )
+        except exceptions.QuitWithoutSaving:
+            raise
+        except SystemExit:  # Save and quit.
+            save_game(handler, "savegame.sav")
+            raise
+        except BaseException:  # Save on any other unexpected exception.
+            save_game(handler, "savegame.sav")
+            raise
 
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
 
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                  player_x += action.dx
-                  player_y += action.dy
-                
-                elif isinstance(action, EscapeAction):
-                  raise SystemExit()
-
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
